bc54882422acab12dbcec448dcfa2690f56af3be
OtaconEvil <otaconeviloficial@gmail.com>
2023-08-07 18:05:03 -0300

    Update Desactivar la optimización de distribución en Windows Update.bat

--- Desactivar la optimización de distribución en Windows Update.bat
+++ Desactivar la optimización de distribución en Windows Update.bat
@@ -1,4 +1,6 @@
 @echo off
+title - https://github.com/OtaconEvil
+
 REM --- Desactivar la optimización de distribución en Windows Update ---
 
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization" /v "DODownloadMode" /t REG_DWORD /d 0 /f
